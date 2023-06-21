b3c76891891138bd7accd88eb102051aa987e96e
Kirubiel Beza <kirubelkeb@gmail.com>
2023-06-20 18:12:41 -0700

    betty style correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,58 +1,43 @@
 #include "main.h"
 /**
- * print_times_table - function that prints the n times table, starting with 0
- * @n: the number passed that decides the table size
- * Return: void
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times
  */
 void print_times_table(int n)
 {
-int i;
-int j;
-int x;
+int i, j, k;
 
-if (n <= 15 && n >= 0)
+if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
-x = i * j;
-if (j > 0)
+k = j * i;
+if (j == 0)
 {
-if (x > 99 && x <= 999)
-{
-_putchar(',');
-_putchar(' ');
-}
-else if (x > 9 && x <= 99)
+_putchar(k + '0');
+} else if (k < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-}
-else
-}
+_putchar(' ');
+_putchar(k + '0');
+} else if (k >= 10 && k < 100)
+{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+} else if (k >= 100)
+{
+_putchar(',');
 _putchar(' ');
-}
-}
-if (x > 99 && x <= 999)
-{
-_putchar((x / 100) + '0');
-_putchar(((x / 10) % 10) + '0');
-_putchar((x % 10) + '0');
-}
-else if (x > 9 && x <= 99)
-{
-_putchar((x / 10) + '0');
-_putchar((x % 10) + '0');
-}
-else
-{
-_putchar(x + '0');
-}
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
