5a5fe3fd35a8720a3e1e1e15c94a757fb17c47bd
Kirubiel Beza <kirubelkeb@gmail.com>
2023-06-20 17:58:57 -0700

    betty style correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,27 +20,39 @@ x = i * j;
 if (j > 0)
 {
 if (x > 99 && x <= 999)
+{
 _putchar(',');
 _putchar(' ');
+}
 else if (x > 9 && x <= 99)
+{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
+}
 else
+}
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 }
+}
 if (x > 99 && x <= 999)
+{
 _putchar((x / 100) + '0');
 _putchar(((x / 10) % 10) + '0');
 _putchar((x % 10) + '0');
+}
 else if (x > 9 && x <= 99)
+{
 _putchar((x / 10) + '0');
 _putchar((x % 10) + '0');
+}
 else
+{
 _putchar(x + '0');
+}
 }
 _putchar('\n');
 }
