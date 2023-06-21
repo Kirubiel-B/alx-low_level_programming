d912e5effd0be07100391bf07cd947b9fd5cb24b
Kirubiel Beza <kirubelkeb@gmail.com>
2023-06-20 18:41:07 -0700

    betty style correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,19 @@ int print_last_digit(int n)
 {
 
 int lastdigit;
+
 if (n < 0)
 {
 n = -n;
 }
 
 lastdigit = n % 10;
+
+if (lastdigit < 0)
+{
+lastdigit = -lastdigit;
+}
+
 _putchar(lastdigit + '0');
 
 return (lastdigit);
