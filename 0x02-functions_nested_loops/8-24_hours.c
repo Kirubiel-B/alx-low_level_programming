faecb65f3d8965d4ca62d426ce31229dcf9c1674
Kirubiel Beza <kirubelkeb@gmail.com>
2023-06-20 18:22:30 -0700

    betty style correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - function that prints every minute of the day, starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day, starting from 00:00 to 23:59
  * Return: void
  */
 void jack_bauer(void)
@@ -12,7 +12,7 @@ int l;
 
 for (i = 0; i < 3; i++)
 {
-for (j = 0; j < 4; j++)
+for (j = 0; j < 10; j++)
 {
 for (k = 0; k < 6; k++)
 {
