4d40db0a4e357177a4cc17fd86f278e6f8463d1e
Kirubiel Beza <kirubelkeb@gmail.com>
2023-06-26 09:49:23 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: the string
+ * Return: lengt
+ */
+int _strlen(char *s)
+{
+int lengt = 0;
+
+while (*s != '\0')
+}
+lengt++;
+s++;
+}
+return (lengt);
+}
